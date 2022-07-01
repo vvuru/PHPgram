7f6892cc489e91408ea57732193b305ca5d4f650
vvuru <wown5398@gmail.com>
2022-07-01 18:09:25 +0900

    220701 meyou youme(2)

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -32,13 +32,19 @@ class UserModel extends Model
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
 
-    public function selUserByIuser(&$param)
+    public function selUserProfile(&$param)
     {
-        $sql = "SELECT iuser, email, nm, cmt, mainimg, regdt 
-                  FROM t_user
-                 WHERE iuser = :iuser";
+        $feediuser = $param["feediuser"];
+        $loginiuser = $param["loginiuser"];
+        $sql = "SELECT iuser, email, nm, cmt, mainimg,
+                        (SELECT COUNT(ifeed) FROM t_feed WHERE iuser={$feediuser}) AS feedcnt,
+                        (SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser={$feediuser} AND toiuser={$loginiuser}) AS youme,
+                        (SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser={$loginiuser} AND toiuser={$feediuser}) AS meyou
+                FROM t_user
+                 WHERE iuser={$feediuser}";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(":iuser", $param["iuser"]);
+        // $stmt->bindValue(":feeduser", $param["feediuser"]);
+        // $stmt->bindValue(":loginiuser", $param["iuser"]);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
