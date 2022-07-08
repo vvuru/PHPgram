c8c307ef9bf59d81a3f9fcb4dc0f984273d83b42
vvuru <wown5398@gmail.com>
2022-07-08 16:42:07 +0900

    +direct message

--- test.php
+++ test.php
@@ -1,6 +1,19 @@
 <?php
-require_once "application/utils/FileUtils.php";
+fscanf(STDIN, "%u\n", $a);
+fscanf(STDIN, "%u", $b);
+//$a=472 $b=385
 
-$result = getRandomFileNm("aaaa_asdfsaf.sdfsdf.png");
+$bArr = str_split($b);
+//$bArr=[3,8,5]
 
-print $result;
+//  $a * $bArr[0] * 10^0
+//+ $a * $bArr[1] * 10^1
+//+ $a * $bArr[2] * 10^2
+
+$sum = 0;
+for ($i = 0; $i < Count($bArr); $i++) {
+    $result = $a * $bArr[$i] * pow(10, $i);
+    fprintf(STDOUT, "%u\n", $result);
+    $sum += $result;
+}
+fprintf(STDOUT, "$u", $sum);
